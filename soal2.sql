1b79fcb614cca5d5145cd23171bf2cb71ff684df
Baktiar Mahardika <baktiarmahardika08@gmail.com>
2024-04-28 20:40:09 +0700

    soal 1, soal 2

---
+++ soal2.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 28, 2024 at 03:39 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_mhs`
+--
+
+CREATE TABLE `data_mhs` (
+  `id` int(11) NOT NULL,
+  `gambar` varchar(50) NOT NULL,
+  `nim` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `notlp` varchar(25) NOT NULL,
+  `email` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `data_mhs`
+--
+
+INSERT INTO `data_mhs` (`id`, `gambar`, `nim`, `nama`, `notlp`, `email`) VALUES
+(1, 'asdf.png', 1928347, 'Dika', '0819238192', 'admin@g.c'),
+(19, 'Hero.png', 2352, 'Ahmad', '089111111', 'baktiarmahardika08@gmail.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_mhs`
+--
+ALTER TABLE `data_mhs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_mhs`
+--
+ALTER TABLE `data_mhs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
